7925e928a804f43e1aa278437810fad6335b4cba
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-24 18:22:45 -0400

    moved api to its own repo

---
+++ api/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    REDDIT_API_CLIENT: "0wEuv55BOL4gMQ",
+    REDDIT_API_SECRET: "z1BZIEIUJfDV80_PfJ8Xd8LpH_8",
+    REDDIT_API_REDIRECT: "http://127.0.0.1:4000/auth/reddit/callback",
+    REDDIT_OAUTH_URI: "https://oauth.reddit.com/api/v1/me",
+    EXPRESS_ALLOW_ORIGIN: "http://127.0.0.1:3000",
+    ORIGIN_HOME: "http://127.0.0.1:3000"
+}
